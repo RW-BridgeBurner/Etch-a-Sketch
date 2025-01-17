1c6a0ea52398d78b1d3dba26cc07f1194a1b5848
RW-BridgeBurner <4577045+RW-BridgeBurner@users.noreply.github.com>
2025-01-17 19:27:43 +0900

    Etch-a-sketch implemented, max size 100x100, random color

--- script.js
+++ script.js
@@ -3,22 +3,60 @@
 
 
 // Reference to parent div:
+
+let gridSize = 0;
+
+while (true) {
+    gridSize = prompt("What grid size would you like between 1 and 100?");
+
+    if (gridSize === null) {
+        break;
+    }
+
+    const number = parseInt(gridSize);
+
+    if (number >=1 && number <= 100) {
+        break;
+    }    
+    else {
+        alert("Please enter a number between 1 and 100")
+    }
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+let color = getRandomColor();
+const containerWidth = 960;
+
+const squareSize = containerWidth / gridSize;
 const container = document.getElementById('container');
 
 // New square divs:
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < gridSize * gridSize; i++) {
     const square = document.createElement('div');
-    //square.textContent = `Div ${i+1}`;
     square.classList.add('box');
-
     container.appendChild(square)
-}
 
-// Mouseover
-const square = document.querySelectorAll('.box');
-square.forEach(square => {
+    // Mouseover
     square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'yellow';
+        square.style.backgroundColor = `${getRandomColor()}`;
     });
-});
+};
+
+const style = document.createElement('style');
+style.innerHTML = `
+    .box {
+        width: ${squareSize}px;
+        height: ${squareSize}px;
+        background-color: ${color};
+    }
+`;
+document.head.appendChild(style);
 
